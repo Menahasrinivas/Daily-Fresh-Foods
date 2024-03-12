f1a3b7a29766759af724dc21827869c56e9578b0
Menahasrinivas <menahasrinivas@gmail.com>
2024-03-12 19:35:45 +0530

    update

---
+++ frontend/src/page/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className = 'bg-green-300 w-full max-w-md-auto h-36 flex justify-center items-center font-bold text-lg'>
+      <p>Srini Fresh Foods</p>
+    </div>
+  )
+}
+
+export default Contact

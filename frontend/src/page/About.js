478f32eaa8f613d0ff39327c8858eebb0811be47
Menahasrinivas <menahasrinivas@gmail.com>
2024-03-15 13:03:49 +0530

    add

--- frontend/src/page/About.js
+++ frontend/src/page/About.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className = 'bg-green-300 w-full max-w-md-auto h-36 flex justify-center items-center font-bold text-lg'>
+      <p>
+        <h1>Daily Fresh foods</h1>
+        <h2>We provide fresh vegetables,Fruits and fresh food as in App.<br>
+        You can choose the items in menu.
+        </br></h2>
+        </p></div>
   )
 }
 
--- frontend/src/page/Contact.js
+++ frontend/src/page/Contact.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
-
-
-
-
 const Contact = () => {
   return (
     <div className = 'bg-green-300 w-full max-w-md-auto h-36 flex justify-center items-center font-bold text-lg'>
-      <p>Srini Fresh Foods</p>
+      <h1>Daily Fresh Foods</h1>
+      <p>12,Anna nagar,Velachery,Chennai.666111.ph.no 9876543210
+      </p>
     </div>
   )
 }
 
-export default Contact
+export default Contact;

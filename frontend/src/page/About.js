cd06995a64e420dc50265a45d4bf7928311a7852
Menahasrinivas <menahasrinivas@gmail.com>
2024-03-15 16:27:32 +0530

    add update

--- frontend/src/page/About.js
+++ frontend/src/page/About.js
@@ -3,12 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div className = 'bg-green-300 w-full max-w-md-auto h-36 flex justify-center items-center font-bold text-lg'>
-      <p>
-        <h1>Daily Fresh foods</h1>
-        <h2>We provide fresh vegetables,Fruits and fresh food as in App.<br>
-        You can choose the items in menu.
-        </br></h2>
-        </p></div>
+      <p>We are provide fresh foods</p>
+        </div>
   )
 }
 

97b34837b19b67297708de454f511c3a49e302a0
Menahasrinivas <menahasrinivas@gmail.com>
2024-03-15 13:27:00 +0530

    last update

--- frontend/src/page/Contact.js
+++ frontend/src/page/Contact.js
@@ -4,8 +4,7 @@ const Contact = () => {
   return (
     <div className = 'bg-green-300 w-full max-w-md-auto h-36 flex justify-center items-center font-bold text-lg'>
       <h1>Daily Fresh Foods</h1>
-      <p>12,Anna nagar,Velachery,Chennai.666111.ph.no 9876543210
-      </p>
+      <h2>12,Anna nagar,Velachery,Chennai.666111.ph.no 9876543210</h2>
     </div>
   )
 }
